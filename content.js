5a91b4bc9d3011a7423f83ba631efdfd63d90185
spddl <spddlhh@gmail.com>
2020-12-17 21:37:40 +0100

    0.0.2

--- content.js
+++ content.js
@@ -7,17 +7,34 @@
 // @grant        none
 // ==/UserScript==
 
-// chrome-extension://aodfehgjdhhkbcbcbmeahkhpkepennil/content.js
-// url:chrome-extension://aodfehgjdhhkbcbcbmeahkhpkepennil/content.js
+const MuteIcon = 'button[data-a-target="player-mute-unmute-button"] path[d="M5 7l4.146-4.146a.5.5 0 01.854.353v13.586a.5.5 0 01-.854.353L5 13H4a2 2 0 01-2-2V9a2 2 0 012-2h1zM12 8.414L13.414 7l1.623 1.623L16.66 7l1.414 1.414-1.623 1.623 1.623 1.623-1.414 1.414-1.623-1.623-1.623 1.623L12 11.66l1.623-1.623L12 8.414z"]'
+const checkMute = () => {
+  if (document.querySelector(MuteIcon) === null) {
+    return false // Volume Up
+  } else {
+    return true // Mute
+  }
+}
 
-let timeoutID
+const checkAds = (targetNode) => {
+  if (targetNode.querySelector('div[data-test-selector="sad-overlay"]') === null) {
+    if (AdsPlaying && checkMute()) {
+      document.querySelector('button[data-a-target="player-mute-unmute-button"]').click()
+      AdsPlaying = false
+    }
+  } else {
+    if (!AdsPlaying && !checkMute()) {
+      document.querySelector('button[data-a-target="player-mute-unmute-button"]').click()
+      AdsPlaying = true
+    }
+  }
+}
+
+let AdsPlaying
 const createEvent = targetNode => {
-  checkAds()
-  const observer = new MutationObserver(() => { // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-    window.clearTimeout(timeoutID)
-    timeoutID = window.setTimeout(() => {
-      checkAds()
-    }, 1000)
+  checkAds(targetNode)
+  const observer = new MutationObserver(async () => { // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    checkAds(targetNode)
   })
   observer.observe(targetNode, {
     childList: true,
@@ -29,20 +46,19 @@ const createEvent = targetNode => {
   })
 }
 
-const checkDiv = () => {
+const checkDiv = (delay = 0) => {
   setTimeout(() => {
-    const CommunityPointsSummary = document.querySelectorAll('div.tw-absolute.avap-ads-container').length !== 0 ? document.querySelectorAll('div.tw-absolute.avap-ads-container')[0] : []
-    if (CommunityPointsSummary instanceof Node) {
-      createEvent(CommunityPointsSummary)
+    const defaultPlayer = document.querySelectorAll('div.video-player__overlay').length !== 0 ? document.querySelectorAll('div.video-player__overlay')[0] : []
+    if (defaultPlayer instanceof Node) {
+      createEvent(defaultPlayer)
     } else {
-      checkDiv()
+      checkDiv(delay + 100 > 1000 ? 1000 : delay + 100)
     }
-  }, 500)
+  }, delay)
 }
 
 if (typeof (chrome) !== 'undefined' && chrome.runtime !== 'undefined') {
   chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    console.log({ msg, sender, sendResponse })
     if (msg.text === 'createEvent') {
       if (window.muteAds) {
         sendResponse(false)
@@ -55,21 +71,4 @@ if (typeof (chrome) !== 'undefined' && chrome.runtime !== 'undefined') {
   })
 } else {
   checkDiv()
-}
-
-const MuteIcon = 'button[data-a-target="player-mute-unmute-button"] path[d="M5 7l4.146-4.146a.5.5 0 01.854.353v13.586a.5.5 0 01-.854.353L5 13H4a2 2 0 01-2-2V9a2 2 0 012-2h1zM12 8.414L13.414 7l1.623 1.623L16.66 7l1.414 1.414-1.623 1.623 1.623 1.623-1.414 1.414-1.623-1.623-1.623 1.623L12 11.66l1.623-1.623L12 8.414z"]'
-const checkMute = () => {
-  if (document.querySelector(MuteIcon) === null) {
-    return false // Volume Up
-  } else {
-    return true // Mute
-  }
-}
-
-const checkAds = () => {
-  if (document.querySelector('span[data-test-selector="ad-banner-default-text"]') !== null && !checkMute()) {
-    document.querySelector('button[data-a-target="player-mute-unmute-button"]').click()
-  } else if (checkMute()) {
-    document.querySelector('button[data-a-target="player-mute-unmute-button"]').click()
-  }
 }
